34f7131d1eefebee5702e67628af86ee97a6da15
duylamnguyen0308 <sangphuoclam@gmail.com>
2024-07-26 16:55:56 +0700

    Buoi20

--- buoi19/src/App.js
+++ buoi19/src/App.js
@@ -1,9 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './components/Weather.js';
+import Header from './components/Header.js'
+import Student from './components/Student.js'
+import Footer from './components/Footer.js'
+import Notfound from './components/Notfound.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Weather />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/product" element={<Student />} />
+          <Route path="/price" element={<Weather />} />
+          <Route path="/contact/:meo" element={<Student />} />
+          <Route path="/contact" element={<Weather />} />
+          <Route path="/*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Weather />
+
+      <Footer /> */}
+
+    </div>
   );
 }
 
